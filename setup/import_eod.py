3f88cd7077122cd09c704e4c85a6c94927bcddef
Daniel LeBlanc <daniel@Daniels-MBP.attlocal.net>
2024-11-13 15:35:47 -0600

    going with polars

--- setup/import_eod.py
+++ setup/import_eod.py
@@ -8,13 +8,18 @@ import polars as pl
 
 
 # read sample data
-sample_date = "2023-10-31"
+sample_date = "2023-03-28"
+y, m, d = map(int, sample_date.split("-"))
 sample_data = "stocks_day_candlesticks_example.csv"
 df = pl.read_csv(sample_data)
+
+
 # convert window_start from unix to datetime & create year/month/day cols
-df = df.with_columns(pl.from_epoch("window_start", time_unit="ms")).with_columns(
-    year=pl.col("window_start").dt.year(),
-    month=pl.col("window_start").dt.month(),
-    day=pl.col("window_start").dt.day(),
+df = df.with_columns(
+    pl.from_epoch("window_start", time_unit="ms"),
+    year=y,
+    month=m,
+    day=d,
 )
 print(df)
+# TODO: write data to database
